7916ecca18619d0a00131c65344c723c580f96a5
Melissa <melissakheying@gmail.com>
2020-08-31 14:35:18 -0500

    Use Formik and Yup for GetStarted validation

--- client/src/components/views/GetStarted.js
+++ client/src/components/views/GetStarted.js
@@ -1,78 +1,75 @@
 import React from 'react'
+import * as Yup from 'yup'
 
+import { Formik, Field, Form, ErrorMessage } from "formik"
 import { Link } from 'react-router-dom'
 
 export const GetStarted = () => {
   return (
-    <>
-      <div className='ml-4 mt-4 getStarted'>
-        <div className="field">
-          <label className="label">Name</label>
-          <div className="control">
-            <input className="input" type="text" placeholder="Text input" />
-          </div>
-        </div>
+<section className='has-text-centered'>
+      <h1 className='title mt-4'>Get Started</h1>
+      <Formik
+        initialValues={{
+          name: '',
+          email: '',
+          username: '',
+          password: '',
+        }}
+        validationSchema={Yup.object({
+          name: Yup.string().required('Name is required'),
+          email: Yup.string().email('invalid email').required('Email is required'),
+          username: Yup.string().required('username is required'),
+          password: Yup.string().min(6).max(12).required("password is required"),
+        })}
+        onSubmit={(values) => {
+          console.log("submission, values")
+        }}
+      >
 
-        <div className="field">
-          <label className="label">Username</label>
-          <div className="control has-icons-left has-icons-right">
-            <input className="input is-success" type="text" placeholder="Text input" value="bulma" />
-            <span className="icon is-small is-left">
-              <i className="fas fa-user"></i>
-            </span>
-            <span className="icon is-small is-right">
-              <i className="fas fa-check"></i>
-            </span>
+        <Form className='has-text-centered mt-4'>
+        <div className='field'>
+            <label htmlFor="name">Name</label>
+            <div className='control'>
+              <Field className='mt-3' name="name" type="text" />
+              <p className='help is-danger'>
+                <ErrorMessage name="name" />
+              </p>
+            </div>
           </div>
-          <p className="help is-success">This username is available</p>
-        </div>
 
-        <div className="field">
-          <label className="label">Email</label>
-          <div className="control has-icons-left has-icons-right">
-            <input className="input is-danger" type="email" placeholder="Email input" value="hello@" />
-            <span className="icon is-small is-left">
-              <i className="fas fa-envelope"></i>
-            </span>
-            <span className="icon is-small is-right">
-              <i className="fas fa-exclamation-triangle"></i>
-            </span>
+          <div className='field'>
+            <label htmlFor="email">Email</label>
+            <div className='control'>
+              <Field className='mt-3' name="email" type="text" />
+              <p className='help is-danger'>
+                <ErrorMessage name="email" />
+              </p>
+            </div>
           </div>
-          <p className="help is-danger">This email is invalid</p>
-        </div>
 
 
-        <div className="field">
-          <div className="control">
-            <label className="checkbox">
-              <input type="checkbox" />
-      I agree to the <a href="#">terms and conditions</a>
-            </label>
+          <div className='field'>
+            <label htmlFor="username">Username</label>
+            <div className='control'>
+              <Field className='mt-3' name="username" type="text" />
+              <p className='help is-danger'>
+                <ErrorMessage name="username" />
+              </p>
+            </div>
           </div>
-        </div>
 
-        <div className="field">
-          <div className="control">
-            <label className="radio">
-              <input type="radio" name="question" />
-      Yes
-    </label>
-            <label className="radio">
-              <input type="radio" name="question" />
-      No
-    </label>
+          <div className='field'>
+            <label htmlFor="password">Password</label>
+            <div className='control'>
+              <Field className='mt-3' name="password" type="password" />
+              <p className='help is-danger'>
+                <ErrorMessage name="password" />
+              </p>
+            </div>
           </div>
-        </div>
-
-        <div className="field is-grouped">
-          <div className="control">
-            <button className="button is-link">Submit</button>
-          </div>
-          <div className="control">
-            <button className="button is-link is-light">Cancel</button>
-          </div>
-        </div>
-      </div>
-    </>
+          <button className="button is-primary" type="submit">Submit</button>
+        </Form>
+      </Formik>
+    </section>
   )
 }
