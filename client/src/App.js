aaa6c3363fabc704dd25777b9c75d81c15396fdf
Melissa <melissakheying@gmail.com>
2020-09-01 11:50:32 -0500

    Place ClientChart above all components for viewing purposes

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import logo from "./logo.svg"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 
 import { ClientChart, Header, Home, Login } from "./components"
-import { GetStarted } from "components/views/GetStarted"
+import { GetStarted } from "components/base/GetStarted"
 
 import "./App.scss"
 
@@ -12,8 +12,10 @@ import "./App.scss"
 function App() {
   return (
     <Router>
+      <ClientChart />
       <Route exact={true} path="/">
         <Home />
+
       </Route>
 
       <Switch>
@@ -24,7 +26,7 @@ function App() {
       </Switch>
 
       <Switch>
-        <Route exact={true} path="/get-started">
+        <Route exact={true} path="/users">
           <Header />
           <GetStarted />
         </Route>
