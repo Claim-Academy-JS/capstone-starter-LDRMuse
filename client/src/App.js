2c0d44cfd398ad640945d99182638ed050ffcc28
Manav Misra <manavm@visionwebsoft.com>
2020-09-01 22:29:17 -0500

    :bug: Correct tags on Router

--- client/src/App.js
+++ client/src/App.js
@@ -9,36 +9,33 @@ import "./App.scss";
 function App() {
   return (
     <Router>
-      <Route exact={true} path="/">
-        <Home />
-      </Route>
-
-      <Route exact={true} path="/client-chart">
-        <Header />
-        <ClientChart />
-      </Route>
-
       <Switch>
+        <Route exact={true} path="/">
+          <Home />
+        </Route>
+
+        <Route exact={true} path="/client-chart">
+          <Header />
+          <ClientChart />
+        </Route>
+
         <Route exact={true} path="/login">
           <Header />
           <Login />
         </Route>
-      </Switch>
 
-      <Switch>
         <Route exact={true} path="/users">
           <Header />
           <GetStarted />
         </Route>
-      </Switch>
 
-      <Switch>
         <Route exact path="/client-chart">
           {/* only if user is logged in */}
         </Route>
       </Switch>
+      <Footer />
     </Router>
-  )
+  );
 }
 
 export default App;
