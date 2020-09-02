8bfb7896b7863dddc41884d9b1148c2777be51f4
Manav Misra <manavm@visionwebsoft.com>
2020-09-01 22:31:25 -0500

    :recycle: Export App as named export for conistency
    Making into arrow fxn.

--- client/src/App.js
+++ client/src/App.js
@@ -6,36 +6,32 @@ import { GetStarted } from "components/base/GetStarted";
 
 import "./App.scss";
 
-function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact={true} path="/">
-          <Home />
-        </Route>
+export const App = () => (
+  <Router>
+    <Switch>
+      <Route exact={true} path="/">
+        <Home />
+      </Route>
 
-        <Route exact={true} path="/client-chart">
-          <Header />
-          <ClientChart />
-        </Route>
+      <Route exact={true} path="/client-chart">
+        <Header />
+        <ClientChart />
+      </Route>
 
-        <Route exact={true} path="/login">
-          <Header />
-          <Login />
-        </Route>
+      <Route exact={true} path="/login">
+        <Header />
+        <Login />
+      </Route>
 
-        <Route exact={true} path="/users">
-          <Header />
-          <GetStarted />
-        </Route>
+      <Route exact={true} path="/users">
+        <Header />
+        <GetStarted />
+      </Route>
 
-        <Route exact path="/client-chart">
-          {/* only if user is logged in */}
-        </Route>
-      </Switch>
-      <Footer />
-    </Router>
-  );
-}
-
-export default App;
+      <Route exact path="/client-chart">
+        {/* only if user is logged in */}
+      </Route>
+    </Switch>
+    <Footer />
+  </Router>
+);
