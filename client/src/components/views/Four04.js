8b8df5aded8421fa0b68a883541a631c3f709fc9
Melissa <melissakheying@gmail.com>
2020-09-02 12:40:45 -0500

    :lipstick: 404 design

--- client/src/components/views/Four04.js
+++ client/src/components/views/Four04.js
@@ -1,14 +1,16 @@
 import React from 'react'
 
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export const Four04 = () => {
   return (
-    <section className='container'>
-      <h2 className='is-1'>404</h2>
-      <p className='has-text-danger'>We cannot find that page!!!
-      <Link to='/'>Wanna go home?</Link>
-      </p>
+    <section className='container has-text-centered'>
+      <h2 className='mt-6 title is-1'>404</h2>
+      <figure className="image container is-128x128 ">
+        <img className="is-rounded" alt="confused404" src="https://png.pngtree.com/png-vector/20190319/ourlarge/pngtree-vector-confused-emoji-icon-png-image_845580.jpg" />
+      </figure>
+      <p className='subtitle is-3 has-text-danger'>Oops, this page does not exist.</p>
+      <p className='subtitle is-2'><Link to='/'>Go back to Homepage</Link></p>
     </section>
   )
 }
