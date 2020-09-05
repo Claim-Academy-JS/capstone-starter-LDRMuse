f8894b6e217f17c07f5bab4f80830ef41eaae5c7
Melissa <melissakheying@gmail.com>
2020-09-05 17:49:21 -0500

    Begin conditional for search client or add new client

--- client/src/components/views/CreateOrSearchClient.js
+++ client/src/components/views/CreateOrSearchClient.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { useHistory, useLocation } from "react-router-dom";
 
 import * as Yup from "yup";
@@ -15,21 +15,27 @@ export const CreateOrSearchClient = () => {
     },
   } = useLocation();
 
+  const [searchClientMode, setSearchClientMode] = useState(
+    location.search.includes("search")
+  );
+
   const clientsAPI = api("clients");
-  //TODO use buttons for conditional
+  //TODO use buttons for conditional searchClients or create client
+  //search clients will lead to chart entry page
   return (
     <Fragment>
       <section className="px-4 py-4 has-text-centered mt-4 mb-4">
         <div className="container">
           <h1 className="title is-3">Hello, {name} !</h1>
           <button className="button is-primary mr-4" type="submit">
-            Add Client
+            Add New Client
           </button>
           <button className="button is-primary" type="submit">
-            Find Client
+            Search Client
           </button>
         </div>
       </section>
+
       <Formik
         initialValues={{
           firstName: "",
@@ -56,55 +62,60 @@ export const CreateOrSearchClient = () => {
           }
         }}
       >
-        <Form className="box ml-2 has-text-centered">
-          <div className="field">
-            <label htmlFor="name">Client Name</label>
-            <div className="control">
-              <Field
-                className="mt-3"
-                name="firstName"
-                type="text"
-                placeholder="First Name"
-              />
-              <p className="help is-danger">
-                <ErrorMessage name="firstName" />
-              </p>
-              <Field
-                className="mt-3"
-                name="lastName"
-                type="text"
-                placeholder="Last Name"
-              />
-              <p className="help is-danger">
-                <ErrorMessage name="lastName" />
-              </p>
-            </div>
-          </div>
+        {({ isSubmitting }) => (
+          <Form className="box ml-2 has-text-centered">
+            {searchClientMode ? <h1>Search Clients</h1> : null}
 
-          <div className="field">
-            <label htmlFor="email">Client Email</label>
-            <div className="control">
-              <Field className="mt-3" name="email" type="text" />
-              <p className="help is-danger">
-                <ErrorMessage name="email" />
-              </p>
-            </div>
-          </div>
+            {!searchClientMode ? (
+              <div className="field">
+                <label htmlFor="name">Client Name</label>
+                <div className="control">
+                  <Field
+                    className="mt-3"
+                    name="firstName"
+                    type="text"
+                    placeholder="First Name"
+                  />
+                  <p className="help is-danger">
+                    <ErrorMessage name="firstName" />
+                  </p>
+                  <Field
+                    className="mt-3"
+                    name="lastName"
+                    type="text"
+                    placeholder="Last Name"
+                  />
+                  <p className="help is-danger">
+                    <ErrorMessage name="lastName" />
+                  </p>
+                </div>
 
-          <div className="field">
-            <label htmlFor="phone">Client Phone</label>
-            <div className="control">
-              <Field className="mt-3" name="phone" type="text" />
-              <p className="help is-danger">
-                <ErrorMessage name="phone" />
-              </p>
-            </div>
-          </div>
+                <div className="field">
+                  <label htmlFor="email">Client Email</label>
+                  <div className="control">
+                    <Field className="mt-3" name="email" type="text" />
+                    <p className="help is-danger">
+                      <ErrorMessage name="email" />
+                    </p>
+                  </div>
+                </div>
 
-          <button className="button is-primary mr-4" type="submit">
-            Add Client
-          </button>
-        </Form>
+                <div className="field">
+                  <label htmlFor="phone">Client Phone</label>
+                  <div className="control">
+                    <Field className="mt-3" name="phone" type="text" />
+                    <p className="help is-danger">
+                      <ErrorMessage name="phone" />
+                    </p>
+                  </div>
+                </div>
+              </div>
+            ) : null}
+            <button className="button is-primary mr-4" type="submit">
+              Add Client
+            </button>
+          </Form>
+        )}
       </Formik>
     </Fragment>
   );
