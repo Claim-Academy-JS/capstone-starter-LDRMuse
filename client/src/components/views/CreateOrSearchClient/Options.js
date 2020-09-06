0a80c2f673fcb2f0e210cc65bf5dbfc7e545e183
Melissa <melissakheying@gmail.com>
2020-09-06 10:07:59 -0500

    Add TODOs for :bug:

--- client/src/components/views/CreateOrSearchClient/Options.js
+++ client/src/components/views/CreateOrSearchClient/Options.js
@@ -2,19 +2,21 @@ import React from "react";
 import PropTypes from "prop-types";
 
 //TODO: get text to fit in buttons
+// TODO: Fix bugs in buttons
+//when add new client is clicked, display add client form
+//when search client id click, display search client form
+//fix bug after certain button is click to display opposite button option
 
-export const Options = ({ searchClientMode, handler }) => {
+export const Options = ({ searchClientMode, handler, addClientMode }) => {
   return (
     <section className="px-4 py-4 has-text-centered mt-4 mb-4">
       <div className="container">
         <button className="button is-primary mr-2" onClick={handler}>
-          {searchClientMode ? "Add New Client?" : "Search Client?"}
+          {addClientMode ? "Add New Client" : "Search Client"}
         </button>
-        {!searchClientMode ? (
-          <button className="button is-primary ml-2" onClick={handler}>
-            {searchClientMode ? "Search Client?" : "Add New Client?"}
-          </button>
-        ) : null}
+        <button className="button is-primary mr-2" onClick={handler}>
+          {searchClientMode ? "Search Client" : "Add New Client"}
+        </button>
       </div>
     </section>
   );
@@ -23,6 +25,7 @@ export const Options = ({ searchClientMode, handler }) => {
 Options.propTypes = {
   searchClientMode: PropTypes.bool,
   handler: PropTypes.func,
+  addClientMode: PropTypes.bool,
 };
 
 Options.defaultProps = { loginMode: false };
