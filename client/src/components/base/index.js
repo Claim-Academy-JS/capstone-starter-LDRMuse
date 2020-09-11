d77ebc3b9d420eb6a2b717491640a51f33aa8ede
Melissa <melissakheying@gmail.com>
2020-09-11 14:33:33 -0500

    move ClientChartEntry into Dashboard Directory

--- client/src/components/base/index.js
+++ client/src/components/base/index.js
@@ -1,2 +1,2 @@
 // If you have some components that need to used from anywhere, you can put them here.
-export { ClientChartEntry } from "./ClientChartEntry";
+export { ClientChartEntry } from "../views/Dashboard/ClientChartEntry";
